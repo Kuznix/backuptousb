478e1f4a7026ae20f6660285c0b7de2bf7126935
kuznix2011 <krzysztofdemirkuzniak@gmail.com>
2026-02-01 10:34:09 +0100

    init

---
+++ build.rs
@@ -0,0 +1,15 @@
+use std::env;
+use std::fs::File;
+use std::io::Write;
+use std::path::Path;
+
+fn main() {
+    // Get current UTC year
+    let year = chrono::Utc::now().year();
+
+    // Write it to a file in OUT_DIR
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let dest_path = Path::new(&out_dir).join("compile_year.rs");
+    let mut f = File::create(&dest_path).unwrap();
+    write!(f, "pub const COMPILE_YEAR: i32 = {};", year).unwrap();
+}
